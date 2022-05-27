92f7649c8f69475e469a59a2b4e7282be7bea58e
Fernanda <espinoza.fernandan@gmail.com>
2022-05-26 17:50:05 -0700

    ahora si

--- hrcalc.py
+++ hrcalc.py
@@ -2,37 +2,36 @@
 
 import numpy as np
 
-# 25 samples per second (in algorithm.h)
+# 25 muestras por segundo
 SAMPLE_FREQ = 25
-# taking moving average of 4 samples when calculating HR
-# in algorithm.h, "DONOT CHANGE" comment is attached
+# utiliza las ultimas 4 muestras para calcular HR
 MA_SIZE = 4
-# sampling frequency * 4 (in algorithm.h)
+# sampling frequency * 4
 BUFFER_SIZE = 100
 
 
-# this assumes ir_data and red_data as np.array
+#  ir_data Y red_data como np.array
 def calc_hr_and_spo2(ir_data, red_data):
     """
     By detecting  peaks of PPG cycle and corresponding AC/DC
     of red/infra-red signal, the an_ratio for the SPO2 is computed.
     """
-    # get dc mean
+    # Promedio DC
     ir_mean = int(np.mean(ir_data))
 
-    # remove DC mean and inver signal
-    # this lets peak detecter detect valley
+    # quita promedio DC e invierte la senial
+    # permite detectar los picos
     x = -1 * (np.array(ir_data) - ir_mean)
 
     # 4 point moving average
-    # x is np.array with int values, so automatically casted to int
+    # x es un np.array con valores enteros
     for i in range(x.shape[0] - MA_SIZE):
         x[i] = np.sum(x[i:i+MA_SIZE]) / MA_SIZE
 
-    # calculate threshold
+    # calcula threshold
     n_th = int(np.mean(x))
-    n_th = 30 if n_th < 30 else n_th  # min allowed
-    n_th = 60 if n_th > 60 else n_th  # max allowed
+    n_th = 30 if n_th < 30 else n_th  # min permitido
+    n_th = 60 if n_th > 60 else n_th  # max permitido
 
     ir_valley_locs, n_peaks = find_peaks(x, BUFFER_SIZE, n_th, 4, 15)
 #     print(ir_valley_locs[:n_peaks], ",", end="")
@@ -44,29 +43,28 @@ def calc_hr_and_spo2(ir_data, red_data):
         hr = int(SAMPLE_FREQ * 60 / peak_interval_sum)
         hr_valid = True
     else:
-        hr = -999  # unable to calculate because # of peaks are too small
+        hr = -999  # valor invalido, no hay suficientes picos
         hr_valid = False
 
     # ---------spo2---------
 
-    # find precise min near ir_valley_locs (???)
+    # encontrar valor minimo preciso cerca de ir_valley_locs (???)
     exact_ir_valley_locs_count = n_peaks
 
-    # find ir-red DC and ir-red AC for SPO2 calibration ratio
-    # find AC/DC maximum of raw
+    # encontrar ir-red DC y ir-red AC para la calibracion del radio SPO2
+    # encontrar AC/DC maximo de valores crudos
 
-    # FIXME: needed??
     for i in range(exact_ir_valley_locs_count):
         if ir_valley_locs[i] > BUFFER_SIZE:
-            spo2 = -999  # do not use SPO2 since valley loc is out of range
+            spo2 = -999  # valor invalido porque los picos estan fuera de rango
             spo2_valid = False
             return hr, hr_valid, spo2, spo2_valid
 
     i_ratio_count = 0
     ratio = []
 
-    # find max between two valley locations
-    # and use ratio between AC component of Ir and Red DC component of Ir and Red for SpO2
+    # encontrar el valor maximo entre dos picos
+    # use ratio between AC component of Ir and Red DC component of Ir and Red for SpO2
     red_dc_max_index = -1
     ir_dc_max_index = -1
     for k in range(exact_ir_valley_locs_count-1):
@@ -83,23 +81,22 @@ def calc_hr_and_spo2(ir_data, red_data):
 
             red_ac = int((red_data[ir_valley_locs[k+1]] - red_data[ir_valley_locs[k]]) * (red_dc_max_index - ir_valley_locs[k]))
             red_ac = red_data[ir_valley_locs[k]] + int(red_ac / (ir_valley_locs[k+1] - ir_valley_locs[k]))
-            red_ac = red_data[red_dc_max_index] - red_ac  # subtract linear DC components from raw
+            red_ac = red_data[red_dc_max_index] - red_ac  # restar componentes lineales de DC de los datos crudos
 
             ir_ac = int((ir_data[ir_valley_locs[k+1]] - ir_data[ir_valley_locs[k]]) * (ir_dc_max_index - ir_valley_locs[k]))
             ir_ac = ir_data[ir_valley_locs[k]] + int(ir_ac / (ir_valley_locs[k+1] - ir_valley_locs[k]))
-            ir_ac = ir_data[ir_dc_max_index] - ir_ac  # subtract linear DC components from raw
+            ir_ac = ir_data[ir_dc_max_index] - ir_ac  # restar componentes lineales de DC de los datos crudos
 
             nume = red_ac * ir_dc_max
             denom = ir_ac * red_dc_max
             if (denom > 0 and i_ratio_count < 5) and nume != 0:
-                # original cpp implementation uses overflow intentionally.
-                # but at 64-bit OS, Pyhthon 3.X uses 64-bit int and nume*100/denom does not trigger overflow
-                # so using bit operation ( &0xffffffff ) is needed
+                # usa operacion bit para no hacer trigger a un overflow
                 ratio.append(int(((nume * 100) & 0xffffffff) / denom))
+#                 print('ratio: '+str(int(((nume * 100) & 0xffffffff) / denom)))
                 i_ratio_count += 1
 
-    # choose median value since PPG signal may vary from beat to beat
-    ratio = sorted(ratio)  # sort to ascending order
+    # utiliza un valor medio porque la senial PPG puede variar de latido a latido
+    ratio = sorted(ratio)  # orden ascendiente
     mid_index = int(i_ratio_count / 2)
 
     ratio_ave = 0
@@ -109,7 +106,6 @@ def calc_hr_and_spo2(ir_data, red_data):
         if len(ratio) != 0:
             ratio_ave = ratio[mid_index]
 
-    # why 184?
     # print("ratio average: ", ratio_ave)
     if ratio_ave > 2 and ratio_ave < 184:
         # -45.060 * ratioAverage * ratioAverage / 10000 + 30.354 * ratioAverage / 100 + 94.845
